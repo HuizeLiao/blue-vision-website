d0b87af38aef0525d02f9ffad74e407c155ba464
HuizeLiao <lesedi8379@gmail.com>
2026-07-23 11:05:59 +0800

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   allowedDevOrigins: ['10.9.46.114'],
+  async redirects() {
+    return [
+      {
+        source: '/AboutUs',
+        destination: '/about',
+        permanent: true,
+      },
+      {
+        source: '/ContactUs',
+        destination: '/contact',
+        permanent: true,
+      },
+      {
+        source: '/Services',
+        destination: '/products',
+        permanent: true,
+      },
+    ]
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
